6ea0dae3952fe36fd97b52b8e3b580ec5e704b27
Anurag Maheshwari <41854065+8769ANURAG@users.noreply.github.com>
2022-09-20 17:34:23 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/PredatorHackerzZ/MessageSearchBot.git /MessageSearchBot
-cd /MessageSearchBot
+git clonehttps://github.com/8769ANURAG/t.git /mdisk-search-bot
+cd /mdisk-search-bot
 pip3 install -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
