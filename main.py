dbaf35094da51aa879407c24f0b3f1293b5d9364
Anurag Maheshwari <41854065+8769ANURAG@users.noreply.github.com>
2022-09-22 05:03:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -49,7 +49,7 @@ async def help_handler(_, event: Message):
 async def inline_handlers(_, event: Message):
     if event.text == '/start':
         return
-    answers = f'**Searching For{event.text} 🔍**'	
+    answers = f'**Searching For "{event.text}" 🔍**'	
     async for message in User.search_messages(chat_id=Config.CHANNEL_ID, limit=10, query=event.text):
         if message.text:
             thumb = False
